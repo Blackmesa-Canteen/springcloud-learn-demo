df28898abf6d4297cad7fbd5ee144737b1ea79d1
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2021-11-22 18:13:48 +0800

    feat: impl Eureka服务治理
    开启eureka server-client 服务注册发现

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Xiaotian
+ * @program cloud-demo
+ * @description
+ * @create 2021-11-22 17:34
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
