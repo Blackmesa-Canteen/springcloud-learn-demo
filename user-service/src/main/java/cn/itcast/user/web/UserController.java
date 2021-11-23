45d34ae5710cc15460662d2641e07c870535ef51
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2021-11-23 15:24:40 +0800

    userservice实现配置从Nacos拉取

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -4,7 +4,11 @@ import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -13,6 +17,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("${pattern.dateformat}")
+    private String dateFormat;
 
     /**
      * 路径： /user/110
@@ -23,5 +30,10 @@ public class UserController {
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
+    }
+
+    @GetMapping("/now")
+    public String now () {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateFormat));
     }
 }
