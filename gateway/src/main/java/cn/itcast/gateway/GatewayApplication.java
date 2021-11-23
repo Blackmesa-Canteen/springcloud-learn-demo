e44c64378fee75321fb10dfb626bc276a0322239
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2021-11-24 01:07:05 +0800

    学习gateway网关初步

---
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xiaotian
+ * @program cloud-demo
+ * @description
+ * @create 2021-11-24 00:26
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
