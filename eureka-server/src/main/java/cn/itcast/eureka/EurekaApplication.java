be6923c30f85721ff0c26721047766fa008c488d
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2021-11-23 12:41:10 +0800

    修复

--- eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -4,13 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @author Xiaotian
- */
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class EurekaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
--- user-service/src/main/java/cn/itcast/user/service/UserService.java
+++ user-service/src/main/java/cn/itcast/user/service/UserService.java
@@ -5,17 +5,11 @@ import cn.itcast.user.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Service
 public class UserService {
 
-
-    private UserMapper userMapper;
-
     @Autowired
-    public void setUserMapper(UserMapper userMapper) {
-        this.userMapper = userMapper;
-    }
+    private UserMapper userMapper;
 
     public User queryById(Long id) {
         return userMapper.findById(id);
